707ecd3b7e93f574be1087207cdb859a4d182aa9
joseDanielRestrepoOrozco <jose.resoro4789@gmail.com>
2023-10-02 22:26:21 -0500

    implementacion relacion n-n

--- src/main/java/com/mslibrary/mslibrary/Controllers/LoanController.java
+++ src/main/java/com/mslibrary/mslibrary/Controllers/LoanController.java
@@ -1,4 +1,61 @@
 package com.mslibrary.mslibrary.Controllers;
 
+import com.mslibrary.mslibrary.Models.Book;
+import com.mslibrary.mslibrary.Models.Loan;
+import com.mslibrary.mslibrary.Models.User;
+import com.mslibrary.mslibrary.Repositories.BookRepository;
+import com.mslibrary.mslibrary.Repositories.LoanRepository;
+import com.mslibrary.mslibrary.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/loans")
 public class LoanController {
+
+    @Autowired
+    private LoanRepository theLoanRepository;
+
+    @Autowired
+    private UserRepository theUserRepository;
+
+    @Autowired
+    private BookRepository theBookRepository;
+
+    @GetMapping("")
+    public List<Loan> index() {
+        return this.theLoanRepository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("user/{user_id}/book/{book_id}")
+    public Loan store(@PathVariable String user_id, @PathVariable String book_id) {
+        User theUser = this.theUserRepository.findById(user_id).orElse(null);
+        Book theBook = this.theBookRepository.findById(book_id).orElse(null);
+        if (theUser != null && theBook != null) {
+            Loan newLoan = new Loan();
+            newLoan.setBook(theBook);
+            newLoan.setUser(theUser);
+            newLoan.setDate(LocalDate.now());
+            return this.theLoanRepository.save(newLoan);
+        } else {
+            return null;
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void destroy(@PathVariable String id) {
+        Loan theLoan = this.theLoanRepository
+                .findById(id)
+                .orElse(null);
+        if (theLoan != null) {
+            this.theLoanRepository.delete(theLoan);
+        }
+    }
 }
--- src/main/java/com/mslibrary/mslibrary/Models/Loan.java
+++ src/main/java/com/mslibrary/mslibrary/Models/Loan.java
@@ -1,10 +1,52 @@
 package com.mslibrary.mslibrary.Models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
 
 @Data
 @Document()
 public class Loan {
 
+    @Id
+    private String _id;
+    private LocalDate date;
+    @DBRef
+    private User user;
+    @DBRef
+    private Book book;
+
+    public Loan() {
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
